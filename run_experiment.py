cc29c64b5572dc968abf59c603d8ee5eb0917a93
Michael Walton <walton.myke@gmail.com>
2021-01-22 22:47:07 +0100

    Update run_experiment.py

--- run_experiment.py
+++ run_experiment.py
@@ -1,28 +1,3 @@
-# coding=utf-8
-# Copyright 2018 The Dopamine Authors and Google LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
-#
-# This file is a fork of the original Dopamine code incorporating changes for
-# the multiplayer setting and the Hanabi Learning Environment.
-#
-"""Run methods for training a DQN agent on Atari.
-
-Methods in this module are usually referenced by |train.py|.
-"""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
